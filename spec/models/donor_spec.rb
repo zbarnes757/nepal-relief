ec14c9a283a82ef754607b5a6ed4081ec2cc0315
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-04-27 12:34:42 -0700

    refactor donor tests

--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Donor, type: :model do
-  it {should validate_presence_of(:name)}
-  it {should validate_presence_of(:email)}
-  it {should validate_presence_of(:country)}
-  it {should validate_uniqueness_of(:email)}
+	it { should validate_presence_of :name }
+	it { should validate_presence_of :email }
+	it { should validate_presence_of :country }
+	it { should validate_uniqueness_of :email }
 end

c297561dd2b1905edaa221946ff304f670348c63
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-04-27 12:59:38 -0700

    add requested resource model and migration

---
+++ app/models/requested_resource.rb
@@ -0,0 +1,2 @@
+class RequestedResource < ActiveRecord::Base
+end
---
+++ db/migrate/20150427195600_create_requested_resources.rb
@@ -0,0 +1,13 @@
+class CreateRequestedResources < ActiveRecord::Migration
+  def change
+    create_table :requested_resources do |t|
+      t.string :name
+      t.integer :quantity
+      t.string :urgency
+      t.boolean :fulfilled
+      t.belongs_to :beneficiary, index: true
+
+      t.timestamps null: false
+    end
+  end
+end

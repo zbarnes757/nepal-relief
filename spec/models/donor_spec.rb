55cc752cbae337f32ab39409375bbd49726568e5
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-04-27 14:05:06 -0700

    add validations to donors and pass tests

--- spec/models/donor_spec.rb
+++ spec/models/donor_spec.rb
@@ -5,5 +5,4 @@ RSpec.describe Donor, type: :model do
 	it { should validate_presence_of :email }
 	it { should validate_presence_of :country }
 	it { should validate_uniqueness_of :email }
-	it { should have_secure_password }
 end

e34174a68181931c8da66568cd76d43dac0a62e4
andy-dev <andres.macedo@gmail.com>
2015-04-27 13:33:45 -0700

    add devise for donors with additional parameters, name, phone & country

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :donors
+
+devise_for :donors
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

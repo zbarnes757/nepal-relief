a88ca495f696591845894a3fa3053f9ba480443e
andy-dev <andres.macedo@gmail.com>
2015-04-28 16:43:35 -0700

    add basic newsfeed

--- app/models/requested_resource.rb
+++ app/models/requested_resource.rb
@@ -1,4 +1,9 @@
 class RequestedResource < ActiveRecord::Base
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_beneficiary }
+
+  # creates call backs for activity record
+
 	belongs_to :beneficiary
 	validates :name, :quantity, :urgency, presence: true
 end

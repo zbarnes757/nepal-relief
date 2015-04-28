d3f1cf250ab768f8009017bdd02539cd5fefa548
Evan Hughes <evanhughes3@gmail.com>
2015-04-28 13:32:20 -0700

    added quantity remaining method to calculate how many resources are left

--- app/models/requested_resource.rb
+++ app/models/requested_resource.rb
@@ -2,4 +2,13 @@ class RequestedResource < ActiveRecord::Base
   has_many :claimed_resources
 	belongs_to :beneficiary
 	validates :name, :quantity, :urgency, presence: true
+
+  def quantity_remaining
+    amount_left = self.quantity
+    self.claimed_resources.each do |resource|
+      amount_left -= resource.quantity
+    end
+    amount_left
+     # = self.quantity - self.claimed_resources[0].quantity
+  end
 end

35756a67089f6da71bfe64d20590270df508418e
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-04-28 17:30:19 -0700

    fix maps to load properly

--- app/assets/javascripts/beneficiary.js
+++ app/assets/javascripts/beneficiary.js
@@ -1,33 +1,35 @@
 (function () {
 
 	var setBeneficiaryMap = function () {
-		var longitude = $("#beneficiary-map").data('long');
-		var latitude = $("#beneficiary-map").data('lat');
+		var $mapContainer = $("#beneficiary-map");
+    if($mapContainer.length){
+			var longitude = $mapContainer.data('long');
+			var latitude = $mapContainer.data('lat');
 
-    L.mapbox.accessToken = 'pk.eyJ1Ijoic2FsdHkiLCJhIjoiN2lJeWI2ayJ9.okjWPSPMRHaMiTPEynHDbQ';
-		beneficiaryMap = L.mapbox.map('beneficiary-map', 'mapbox.streets').setView([ latitude, longitude ], 15);
+	    L.mapbox.accessToken = 'pk.eyJ1Ijoic2FsdHkiLCJhIjoiN2lJeWI2ayJ9.okjWPSPMRHaMiTPEynHDbQ';
+			beneficiaryMap = L.mapbox.map('beneficiary-map', 'mapbox.streets').setView([ latitude, longitude ], 15);
 
-		var geoData = [{
-      "type": "FeatureCollection",
-      "features": [{
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [ longitude, latitude ]
-      },
-      "properties": {
-        "marker-symbol": "library",
-        "marker-size": "medium",
-        "marker-color": "#d27591",
-      }
-    }],
-    }];
+			var geoData = [{
+	      "type": "FeatureCollection",
+	      "features": [{
+	      "type": "Feature",
+	      "geometry": {
+	        "type": "Point",
+	        "coordinates": [ longitude, latitude ]
+	      },
+	      "properties": {
+	        "marker-symbol": "library",
+	        "marker-size": "medium",
+	        "marker-color": "#d27591",
+	      }
+	    }],
+	    }];
 
-    var myLayer = L.mapbox.featureLayer().addTo(beneficiaryMap);
-    myLayer.setGeoJSON(geoData);
+	    var myLayer = L.mapbox.featureLayer().addTo(beneficiaryMap);
+	    myLayer.setGeoJSON(geoData);
 
+		}
 	}
-
 
 
 	$(function() {

f3c0a69662cdb84e7dd342838ddd509a896bdac8
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-04-28 22:07:29 -0700

    fix routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resource :requested_resource
   end
 
+  resources :donors
+
   devise_for :donors
 
   # The priority is based upon order of creation: first created -> highest priority.

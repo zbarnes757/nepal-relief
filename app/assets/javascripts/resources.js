01b1c27d74a137c49a5c33ff254e84c00398b968
Zac Barnes <zbarnes@qubiclemonkey.com>
2015-04-30 12:00:51 -0700

    fix naming convention of jQuery variables

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -3,51 +3,51 @@ $(document).ready(function(){
   $(".resource-edit").on("click", function(event){
     event.preventDefault();
 
-    var currentRow = $(this).closest("tr");
+    var $currentRow = $(this).closest("tr");
 
-    currentRow.css("background", "yellow");
+    $currentRow.css("background", "yellow");
 
     //define variables
-    var name = currentRow.find(".resource-name");
-    var quantity = currentRow.find(".resource-quantity");
-    var urgency = currentRow.find(".resource-urgency");
-    var fulfilled = currentRow.find(".resource-fulfilled");
-    var notes = currentRow.find(".resource-notes");
-    var edit = currentRow.find(".resource-edit");
-    var deleteButton = currentRow.find(".resource-delete").html();
+    var $name = $currentRow.find(".resource-name");
+    var $quantity = $currentRow.find(".resource-quantity");
+    var $urgency = $currentRow.find(".resource-urgency");
+    var $fulfilled = $currentRow.find(".resource-fulfilled");
+    var $notes = $currentRow.find(".resource-notes");
+    var $edit = $currentRow.find(".resource-edit");
+    var $deleteButton = $currentRow.find(".resource-delete").html();
 
 
 
-    var text = name.text();
-    name.html("<input type='text'>")
-    name.children().first().val(text)
+    var text = $name.text();
+    $name.html("<input type='text'>")
+    $name.children().first().val(text)
 
-    text = quantity.text()
-    quantity.html("<input type='number'/>")
-    quantity.find("input").val(text)
+    text = $quantity.text()
+    $quantity.html("<input type='number'/>")
+    $quantity.find("input").val(text)
 
-    urgency.html("<select><option value='Low'>Low</option><option value='Medium'>Medium</option><option value='high'>High</option></select>")
+    $urgency.html("<select><option value='Low'>Low</option><option value='Medium'>Medium</option><option value='high'>High</option></select>")
 
-    [name, quantity, urgency, notes].forEach(function(resourceInfo){
+    [$name, $quantity, $urgency, $notes].forEach(function(resourceInfo){
       var text = resourceInfo.text();
       resourceInfo.html("<input type='text'>")
       resourceInfo.children().first().val(text)
     })
 
 
-    currentRow.find(".resource-edit").html("");
-    currentRow.find(".resource-delete").html("<a href='#' class='done'>Done</a>")
+    $currentRow.find(".resource-edit").html("");
+    $currentRow.find(".resource-delete").html("<a href='#' class='done'>Done</a>")
 
 
 
     $(".done").on("click", function(event){
       event.preventDefault();
-      var url = "/beneficiaries/" + currentRow.attr("id") + "/requested_resource";
+      var url = "/beneficiaries/" + $currentRow.attr("id") + "/requested_resource";
 
-      var updateName = name.children().first().val();
-      var updateQuantity = quantity.children().first().val();
-      var updateUrgency = urgency.children().first().val();
-      var updateNotes = notes.children().first().val();
+      var updateName = $name.children().first().val();
+      var updateQuantity = $quantity.children().first().val();
+      var updateUrgency = $urgency.children().first().val();
+      var updateNotes = $notes.children().first().val();
 
 
       $.ajax({
@@ -59,14 +59,14 @@ $(document).ready(function(){
                notes: updateNotes, }
       })
       .done(function(response){
-        currentRow.css("background", "white")
+        $currentRow.css("background", "white")
         console.log(response)
-        name.html(response.name)
-        quantity.html(response.quantity)
-        urgency.html(response.urgency)
-        notes.html(response.notes)
-        edit.html("<a href='#'>Edit</a>")
-        currentRow.find(".resource-delete").html(deleteButton)
+        $name.html(response.name)
+        $quantity.html(response.quantity)
+        $urgency.html(response.urgency)
+        $notes.html(response.notes)
+        $edit.html("<a href='#'>Edit</a>")
+        $currentRow.find(".resource-delete").html($deleteButton)
 
 
       })

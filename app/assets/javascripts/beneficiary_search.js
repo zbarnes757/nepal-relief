71d353413e3a438e21caf60f19573f3fd3b83673
Evan Hughes <evanhughes3@gmail.com>
2015-05-01 16:22:17 -0700

    changed logic in js file and added function to display no results

--- app/assets/javascripts/beneficiary_search.js
+++ app/assets/javascripts/beneficiary_search.js
@@ -5,17 +5,29 @@ $(document).ready(function() {
     var searchText = $(this).val().toLowerCase();
     // Search all name fields to see if they include that text
     var allNames = $('.beni-name');
+    var $allRows = $('.beni-row');
     // Filter everything that returns false
     for (var i = 0; i < allNames.length; i++) {
       if ($(allNames[i]).text().toLowerCase().includes(searchText) === false ) {
-        $(allNames[i]).parent().hide();
+        $(allNames[i]).parent().addClass('hide');
       } else {
-        $(allNames[i]).parent().show();
+        $(allNames[i]).parent().removeClass('hide');
+      }
+
+      if (noResults().length === $allRows.length) {
+        $('.no-results').show();
+      } else {
+        $('.no-results').hide();
       }
 
     };
 
-  })
+  });
 
 
 });
+
+function noResults () {
+  var results = $('tr').filter('.hide');
+  return results;
+}
